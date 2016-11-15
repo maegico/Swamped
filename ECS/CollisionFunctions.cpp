2255b465dc65fed0c6153ad58c69737c59376625
Mike Teixeira <mat8885@ad.rit.edu>
2016-11-15 10:22:27 -0500

    Comments, second test object

--- ECS/CollisionFunctions.cpp
+++ ECS/CollisionFunctions.cpp
@@ -1,6 +1,7 @@
 #include "CollisionFunctions.h"
 #include "Game.h"
 
+//Returns a vector of function pointers that correspond to the given collision masks
 vector<CollisionFunction> CollisionFunctions::GetCollisionFunction(CollisionMask cm1, CollisionMask cm2) {
 	vector<CollisionFunction> returnVector;
 	if (cm1 & 1 << CollisionType::none || cm2 & 1 << CollisionType::none)
@@ -8,12 +9,14 @@ vector<CollisionFunction> CollisionFunctions::GetCollisionFunction(CollisionMask
 	return returnVector;
 }
 
+//returns a vector of all collision functions
 vector<CollisionFunction> CollisionFunctions::GetAllCollisionFunctions() {
 	return{
 		&NoOpCollision
 	};
 }
 
+//Removes both colliding entities
 void CollisionFunctions::NoOpCollision(Game * g, EntityId entityId1, EntityId entityId2, float dt) {
 	g->QueueRemoveEntity(entityId1);
 	g->QueueRemoveEntity(entityId2);
