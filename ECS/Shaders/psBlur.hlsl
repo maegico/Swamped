dbcfcaa6791e9a80d1621f34f2802df59166e4ce
Mike Teixeira <miketeixeira95@gmail.com>
2016-12-11 03:39:22 -0500

    Blur adjustments & horizontal/vertical blur
    I discovered the blur shader wasn't calculating number of samples
    correctly, so I fixed it. I've also added a horizontal/vertical blur,
    but the performance difference is completely negligible at the settings
    we're using. The two can be switched with a define.

--- ECS/Shaders/psBlur.hlsl
+++ ECS/Shaders/psBlur.hlsl
@@ -1,3 +1,5 @@
+#define FULL_BLUR
+
 cbuffer Data : register(b0)
 {
 	float pixelWidth;
@@ -18,11 +20,12 @@ SamplerState Sampler	: register(s0);
 float4 main(VertexToPixel input) : SV_TARGET
 {
 	float4 totalColor = float4(0,0,0,0);
-	uint numSamples = blurAmount * blurAmount * 4;	//taking this out of the for loop
+#ifdef FULL_BLUR
+	uint numSamples = 4*(blurAmount*blurAmount+blurAmount)+2;	//taking this out of the for loop
 
-	for (int y = -blurAmount; y < blurAmount; y++)
+	for (int y = -blurAmount; y <= blurAmount; y++)
 	{
-		for (int x = -blurAmount; x < blurAmount; x++)
+		for (int x = -blurAmount; x <= blurAmount; x++)
 		{
 			//figure out this pixel's uv
 			float2 uv = input.uv + float2(x * pixelWidth, y * pixelHeight);
@@ -31,6 +34,24 @@ float4 main(VertexToPixel input) : SV_TARGET
 			//numSamples++;
 		}
 	}
+
+#else
+
+	uint numSamples = blurAmount * 4 + 1;
+
+	for (int x = -blurAmount; x <= blurAmount; x++) {
+		float2 uv = input.uv + float2(x*pixelWidth, 0);
+		totalColor += (1 - (x*x) / (blurAmount*blurAmount)) * BrightPixels.Sample(Sampler, uv);
+	}
+	for (int y = -blurAmount; y < 0; y++) {
+		float2 uv = input.uv + float2(0, y*pixelWidth);
+		totalColor += (1 - (y*y) / (blurAmount*blurAmount)) * BrightPixels.Sample(Sampler, uv);
+	}
+	for (y = 1; y <= blurAmount; y++) {
+		float2 uv = input.uv + float2(0, y*pixelWidth);
+		totalColor += (1 - (y*y) / (blurAmount*blurAmount)) * BrightPixels.Sample(Sampler, uv);
+	}
+#endif
 
 	return totalColor / numSamples;
 }
